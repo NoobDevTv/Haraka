0e76a09651e786bb712b30fae42935c57c5b266a
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2020-04-19 21:55:28 +0200

    Add Blazor WebApp
    * Add a webassembly project for a webapp and add a simple map and a hexagon logic for simple green hexagons. Try to implement nice looking streets between the hexagons

--- Haraka.WebApp/Server/Program.cs
+++ Haraka.WebApp/Server/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Haraka.Web
+namespace Haraka.WebApp.Server
 {
     public class Program
     {
@@ -21,8 +21,8 @@ namespace Haraka.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
-                    .UseStartup<Startup>()
-                    .UseKestrel();
+                        .UseStartup<Startup>()
+                        .UseKestrel();
                 });
     }
 }
